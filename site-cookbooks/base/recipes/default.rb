97bf9d4668529ad273c01b5d167d94928237b01a
tjinjin <tjinjinprogram@gmail.com>
2016-09-05 23:24:39 +0900

    Add base

---
+++ site-cookbooks/base/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'iostat'
+package 'redis-cli'
+package 'sysstat'
+package 'bind-util'
+package 'lsof'
+package 'tcpdump'
+package 'wget'
